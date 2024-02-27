7a86b02ccb43c3f16210d5a9dfb80345208c43f6
Tom Harvey <tom@alush.co.uk>
2024-02-27 08:29:59 +0000

    removing the extra dot

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
 #!/usr/bin/env python3
-import os
 
 import aws_cdk as cdk
 
-from integration_tests_with_cdk_and_python.integration_tests_with_cdk_and_python_stack import IntegrationTestsWithCdkAndPythonStack
-
+from integration_tests_with_cdk_and_python.hello_world_stack import HelloWorldStack
 
 app = cdk.App()
-IntegrationTestsWithCdkAndPythonStack(app, "IntegrationTestsWithCdkAndPythonStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+HelloWorldStack(
+    app,
+    "HelloWorldStack",
+)
 
 app.synth()
--- tests/unit/test_integration_tests_with_cdk_and_python_stack.py
+++ tests/unit/test_integration_tests_with_cdk_and_python_stack.py
@@ -1,14 +1,16 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from integration_tests_with_cdk_and_python.integration_tests_with_cdk_and_python_stack import IntegrationTestsWithCdkAndPythonStack
+from integration_tests_with_cdk_and_python.hello_world_stack import HelloWorldStack
+
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in integration_tests_with_cdk_and_python/integration_tests_with_cdk_and_python_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = IntegrationTestsWithCdkAndPythonStack(app, "integration-tests-with-cdk-and-python")
+    stack = HelloWorldStack(app, "HelloWorldStack")
     template = assertions.Template.from_stack(stack)
+
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
